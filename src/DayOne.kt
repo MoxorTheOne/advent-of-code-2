c922bf9483fa866bd2f7888e0b1ec8614b784c66
Maximiliano Cambiaso <maximiliano.cambiaso@etermax.com>
2022-12-02 12:56:32 -0300

    Day one refactor

--- src/DayOne.kt
+++ src/DayOne.kt
@@ -2,8 +2,7 @@ suspend fun main() {
     val elves = readRawInput("Day01")
         .split("\n\n")
         .map { elf -> elf.lines().map { it.toInt() }.sum() }
-        .toMutableList()
-    elves.sortDescending()
+        .sortedDescending()
 
     println("Part One ${elves.maxOf { a -> a }}")
     println("Part two ${elves.take(3).sum()}")
