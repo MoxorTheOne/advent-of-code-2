de2fc43e54116035467aad01d0937db7be282ce5
Maximiliano Cambiaso <maximiliano.cambiaso@etermax.com>
2022-12-02 16:38:11 -0300

    Day 2 Pert 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -5,14 +5,56 @@ suspend fun main() {
     //shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
     //plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
 
-    val result = readInput("Day02")
-        .map { it.split(" ") }
-        .map { roundPoints(it[0], it[1]) }
-        .sum()
+    //Opp, Me
+    //A Y(Draw) -   X
+    //B X(Lose) -   X
+    //C Z(Win)  -   X
 
-    println("Part1 $result")
+    val input = readInput("Day02")
+        .map { it.split(" ") }
+
+    println("Part 1 ${partOne(input)}")
+
+    println("Part 2 " + partOne(input.map { swapMyInput(it) }))
 
 }
+
+private fun swapMyInput(input: List<String>): List<String> {
+    when (input[1]) {
+        "X" -> return listOf(input[0], lose(input[0]))
+        "Y" -> return listOf(input[0], draw(input[0]))
+    }
+    //Z
+    return listOf(input[0], win(input[0]))
+}
+
+fun win(to: String): String {
+    when (to) {
+        "A" -> return "Y" //Rock - Paper
+        "B" -> return "Z" //Paper - Scissors
+    }
+    return "X"//Scissors - Rock
+}
+
+fun draw(to: String): String {
+    when (to) {
+        "A" -> return "X"//Rock - Rock
+        "B" -> return "Y"//Paper - Paper
+    }
+    return "Z"//Scissors - Scissors
+}
+
+fun lose(to: String): String {
+    when (to) {
+        "A" -> return "Z"//Rock - Scissors
+        "B" -> return "X"//Paper - Rock
+    }
+    return "Y"//Scissors - Paper
+}
+
+private fun partOne(input: List<List<String>>) = input
+    .map { roundPoints(it[0], it[1]) }
+    .sum()
 
 fun roundPoints(opponent: String, me: String): Int = shapeValue(me) + victoryPoints(opponent, me)
 
