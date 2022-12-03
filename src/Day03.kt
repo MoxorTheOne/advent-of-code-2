1793721168b14cd74436fe23d326cbc5985667e5
Max <maxcambiaso@gmail.com>
2022-12-03 11:27:32 -0300

    Day 3 part two

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,12 +1,25 @@
-suspend fun main() {
-    val output = readInput("Day03")
-            .map { listOf(it.substring(0, (it.length / 2)), it.substring(it.length / 2, it.length)) }
-            .map { findCommon(it) }
-            .map { values.indexOf(it) +1 }
-
-
-    println("Part One ${output.sum()}")
+fun main() {
+    val input = readInput("Day03")
+    println("Part One ${partOne(input)}")
+    println("Part Two ${PartTwo(input)}")
 }
+
+private fun PartTwo(input: List<String>) = input.map { it.toCharArray() }
+        .chunked(3)
+        .map { findBadgeForGroup(it) }
+        .map { values.indexOf(it) + 1 }
+        .sum()
+
+fun findBadgeForGroup(lists: List<CharArray>): Char {
+    val result = lists[0].find { char -> lists[1].contains(char) && lists[2].contains(char) }
+    return result!! //Should fail if not found
+}
+
+private fun partOne(readInput: List<String>) = readInput
+        .map { listOf(it.substring(0, (it.length / 2)), it.substring(it.length / 2, it.length)) }
+        .map { findCommon(it) }
+        .map { values.indexOf(it) + 1 }
+        .sum()
 
 fun findCommon(input: List<String>): Char {
     val lists = input.map { it.toCharArray() }
