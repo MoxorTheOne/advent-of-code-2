224cd97ab49b39cb7475c0a3380bb3916f609512
Maximiliano Cambiaso <maximiliano.cambiaso@etermax.com>
2022-12-04 11:23:12 -0300

    Day 4 refactor

--- src/Day04.kt
+++ src/Day04.kt
@@ -12,10 +12,13 @@ fun pairToElves(pair: List<String>): List<Elve> = pair
     .map { Elve(it[0].toInt(), it[1].toInt()) }
 
 data class Elve(val from: Int, val to: Int) {
-    fun overlaps(other: Elve): Boolean {
-        return IntRange(from, to).contains(other.from) || IntRange(from, to).contains(other.to) ||
-                IntRange(other.from, other.to).contains(from) || IntRange(other.from, other.to).contains(to)
-    }
+    private var range: IntRange = IntRange(from, to)
+
+    fun overlaps(other: Elve): Boolean =
+        range.contains(other.from) ||
+                range.contains(other.to) ||
+                other.range.contains(from) ||
+                other.range.contains(to)
 
     fun fullOverlaps(other: Elve): Boolean = this.contains(other) || other.contains(this)
     fun contains(other: Elve): Boolean = from <= other.from && to >= other.to
