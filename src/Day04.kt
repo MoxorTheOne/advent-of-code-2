3f6052e125e270ae83c257b39b861b40de79ab44
Maximiliano Cambiaso <maximiliano.cambiaso@etermax.com>
2022-12-04 11:09:55 -0300

    Day 4 part two

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,10 +2,9 @@ fun main() {
     val input = readInput("Day04")
         .map { it.split(",") }
         .map { pairToElves(it) }
-        .filter { it[0].overlaps(it[1]) }
 
-    println("Part One ${input.count()}")
-//    println("Part Two ${partTwo(input)}")
+    println("Part One ${input.count { it[0].fullOverlaps(it[1]) }}")
+    println("Part Two ${input.count { it[0].overlaps(it[1]) }}")
 }
 
 fun pairToElves(pair: List<String>): List<Elve> = pair
@@ -13,6 +12,11 @@ fun pairToElves(pair: List<String>): List<Elve> = pair
     .map { Elve(it[0].toInt(), it[1].toInt()) }
 
 data class Elve(val from: Int, val to: Int) {
-    fun overlaps(other: Elve): Boolean = this.contains(other) || other.contains(this)
+    fun overlaps(other: Elve): Boolean {
+        return IntRange(from, to).contains(other.from) || IntRange(from, to).contains(other.to) ||
+                IntRange(other.from, other.to).contains(from) || IntRange(other.from, other.to).contains(to)
+    }
+
+    fun fullOverlaps(other: Elve): Boolean = this.contains(other) || other.contains(this)
     fun contains(other: Elve): Boolean = from <= other.from && to >= other.to
 }
